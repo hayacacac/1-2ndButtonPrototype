412a540c5c861dc576eb80ace399e3e2f794f2ef
hayacacac <96822592+hayacacac@users.noreply.github.com>
2023-09-21 14:23:46 +0900

    チェックポイント機能実装

---
+++ Assets/Scripts/Mechanics/TouchCheckPoint.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Platformer.Mechanics
+{
+    /// <summary>
+    /// キャラクターがチェックポイントに到達したらリスポーン地点更新
+    /// </summary>
+    public class TouchCheckPoint : MonoBehaviour
+    {
+        [SerializeField]
+        private GameController gameController;
+        private Transform spawnPoint;
+        bool isTouched;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            isTouched = false;
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (!isTouched && (other.CompareTag("Player") || other.CompareTag("Muteki")))
+            {
+                gameController.model.spawnPoint = this.transform;
+                isTouched = true;
+            }
+        }
+    }
+}
