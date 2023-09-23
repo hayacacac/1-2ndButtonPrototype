6eff982d45898091dc89317c2cd0de1b94036770
hayacacac <96822592+hayacacac@users.noreply.github.com>
2023-09-23 20:04:50 +0900

    敵がプレイヤーに弾を飛ばすようにした

--- Assets/Scripts/Mechanics/BulletControllers/BulletController.cs
+++ Assets/Scripts/Mechanics/BulletControllers/BulletController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 namespace Platformer.Mechanics
 {
+    /// <summary>
+    /// 何かに当たったら消える弾
+    /// </summary>
     public class BulletController : AttackObject
     {
         void OnCollisionEnter2D(Collision2D collision)
