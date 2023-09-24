aa25f34146d9a9a2f635db52e096a6eea16295e6
hayacacac <96822592+hayacacac@users.noreply.github.com>
2023-09-24 15:05:34 +0900

    近接の生成位置調整

--- Assets/Scripts/ButtonActions/CloseAttack.cs
+++ Assets/Scripts/ButtonActions/CloseAttack.cs
@@ -16,26 +16,29 @@ namespace Platformer.Mechanics
         float attackRange = 0.5f;
         [SerializeField]
         int damage = 1;
+        Vector3 offsetY = new Vector3(0,-0.5f,0);
         
         public override void Perform(PlayerController player){
             //攻撃開始位置のオフセット
-            Vector3 offset = new Vector3(0.25f,0,0);
+            Vector3 offsetX = new Vector3(0.25f,0,0);
             // 左向きのときの斑点が複数かからないように毎回スピードを初期化
             float speed = attackSpeed;
 
             //左向きの時は攻撃方向を逆にする
             if (player.getterFlipX()){
-                offset *= -1;
+                offsetX *= -1;
                 speed *= -1;
             }
 
             // あたり判定オブジェクト生成
-            GameObject closeAttack = Object.Instantiate(closeAttackPrefab, player.transform.position+offset, player.transform.rotation);
+            GameObject closeAttack = Object.Instantiate(closeAttackPrefab, player.transform.position+offsetX+offsetY, player.transform.rotation);
             closeAttack.GetComponent<AttackObject>().damage = damage;
 
             //飛ばす距離を渡す
             CloseAttackController cac = closeAttack.GetComponent<CloseAttackController>();
             cac.range = attackRange;
+            cac.player = player.gameObject.transform;
+            cac.offsetY = offsetY.y;
 
             // プレイヤーの向いている方に飛ばす
             Rigidbody2D rb = closeAttack.GetComponent<Rigidbody2D>();
