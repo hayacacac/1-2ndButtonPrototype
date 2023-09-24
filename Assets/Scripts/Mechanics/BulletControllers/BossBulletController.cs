f663953340325ff399870febd66c26a2b5ee250c
hayacacac <96822592+hayacacac@users.noreply.github.com>
2023-09-24 21:15:32 +0900

    BossAttack

---
+++ Assets/Scripts/Mechanics/BulletControllers/BossBulletController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Platformer.Mechanics
+{
+    /// <summary>
+    /// 敵が攻撃に使う弾のクラス
+    /// </summary>
+    public class BossBulletController : AttackObject
+    {
+        private Rigidbody2D rb;
+        public float slowdownRate = 0.95f;
+
+        void Start(){
+            rb = GetComponent<Rigidbody2D>();
+        }
+        void Update(){
+            if (rb.velocity.magnitude > 0.01f)
+            {
+                rb.velocity -= rb.velocity.normalized * slowdownRate * Time.fixedDeltaTime;
+            }
+            else
+            {
+                rb.velocity = Vector2.zero;
+            }
+        }
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            // 敵、弾、Cinemachineのコライダも無視
+            if (other.CompareTag("Enemy") || other.CompareTag("Muteki") || other.CompareTag("Bullet") || other.CompareTag("EnemyBullet") || other.CompareTag("Cinemachine"))
+            {
+                return;
+            }
+
+            Destroy(this.gameObject); 
+        }
+    }
+}
