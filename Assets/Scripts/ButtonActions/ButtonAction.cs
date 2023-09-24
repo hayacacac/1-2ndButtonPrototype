e39f7429ca843f369a7d9c9dd7728d6cade26ade
Eifu Narita <85473920+n1910496@users.noreply.github.com>
2023-09-24 19:24:30 +0900

    playerのanimation完成
    たぶん

--- Assets/Scripts/ButtonActions/ButtonAction.cs
+++ Assets/Scripts/ButtonActions/ButtonAction.cs
@@ -12,7 +12,7 @@ namespace Platformer.Mechanics
         public Animator animator;
         public string animationName;
         public virtual void Perform(PlayerController player){
-            if(animator != null) animator.SetBool(animationName, true);
+            if(animator != null) animator.SetTrigger(animationName);
         }
     }
 }
